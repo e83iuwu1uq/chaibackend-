69f88c811a8abfd872024d967d5b89356a9386f5
e83iuwu1uq <84569199+e83iuwu1uq@users.noreply.github.com>
2023-12-21 20:39:16 +0530

    add subscriptions model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -188,7 +188,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.
     refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken) {
+    if(!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
     
